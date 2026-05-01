26ab5c420131cfa3b7120914ddc6ad1b3861158a
The-Parthib <rijupanja81@gmail.com>
2026-05-01 23:04:48 +0530

    Add multi-line print example in day1.py

---
+++ Assingments/day_1.py
@@ -0,0 +1,13 @@
+# Multi Line print with one print statement
+
+# print('''
+#       Twinkle, twinkle, little star,
+#       How I wonder what you are!
+
+#         Up above the world so high,
+#         Like a diamond in the sky.
+#         Twinkle, twinkle, little star,
+#         How I wonder what you are
+#       ''')
+
+
