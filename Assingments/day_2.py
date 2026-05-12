ec22bc1d25855f807c88f49b490108df60519dc0
The-Parthib <rijupanja81@gmail.com>
2026-05-12 22:52:14 +0530

    Add program to add two numbers in day_2.py

---
+++ Assingments/day_2.py
@@ -0,0 +1,4 @@
+# write a program to add two numbers and print the result
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("The sum of", a, "and", b, "is", a + b)
